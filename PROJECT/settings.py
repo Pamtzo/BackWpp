28df3c216f88b2c1f2111abb0842465030839acd
Andres Felipe Ospina GUalteros <andresfelipe.ospina@utp.edu.co>
2020-08-14 02:20:26 -0500

    conf server

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!4#rlzomaz+v!zsc3=vlfujiyn+7^3j6j8yl8ovv-*czjh_!9a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','18.219.58.163']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
